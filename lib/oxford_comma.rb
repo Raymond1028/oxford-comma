44f9db41bf8eba5c35ca8bb981c437d98d429c88
Raymond1028 <lancelot53.rs@gmail.com>
2021-12-04 20:10:20 -0500

    Completed Assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,26 @@
 def oxford_comma(array)
+array[0]
+#array.join(" and ")
 
-end
+
+# array.join(' and ') if array.size > 2 # inline conditional
+#   array[-1] = "and " + array[-1]
+#  
+
+   if array.size > 1 && array.size < 3
+      array.join(" and ")
+
+   elsif array.size >= 3
+      array[-1].insert(0, "and ")
+      array.join(', ')
+   else
+       array[0]
+
+   end
+  # array.join()
+   #
+end   
+
+
+
+
